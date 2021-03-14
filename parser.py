7c5ec67c2704bcc63ffc71ce943fc54d94ebc9ad
FrankPujo <70267733+FrankPujo@users.noreply.github.com>
2021-03-14 17:32:19 +0100

    Improvements
    Better variable declaration and reassignation.
    Inizialized for loop.

--- parser.py
+++ parser.py
@@ -1,5 +1,9 @@
 import sys
 
+# check if reading a function or a loop
+inFunct = False
+inLoop = False
+
 # assign a value to a variable
 def parseAssignVar( content ):
 	tokens = content.split( " " )
@@ -10,13 +14,24 @@ def parseAssignVar( content ):
 			if i != 0 and i != 1 and i != 2:
 				operation += tokens[i]
 		value = eval( operation )
-	else:
+	elif tokens[3][:1] == "\"":
 		value = ""
 		for i in range( len( tokens ) ):
 			if i != 0 and i != 1 and i != 2:
 				value += tokens[i]
 				value += " "
 		value = value.lstrip("\"").rstrip(" ").rstrip("\"")
+	elif memory[tokens[3]] is not None:
+		if tokens[5] is not None:
+			if tokens[5].isdigit():
+				result = str(memory[tokens[3]]) + tokens[4] + tokens[5]
+				value = eval( result )
+			elif memory[tokens[5]] is not None:
+				result = str(memory[tokens[3]]) + tokens[4] + str(memory[tokens[5]])
+				value = eval( result )
+
+		else:
+			value = memory[tokens[3]]
 	memory[ identifier ] = value
 
 # print statement
@@ -27,6 +42,13 @@ def printStatement( content ):
 		print( stuff[1:-1] )
 	else:
 		print( memory[ stuff ] )
+
+# parse for loops
+def startLoop( content ):
+	inLoop = True
+	tokens = content.split( " " )
+	repeating = memory[ tokens[1] ]
+	print( repeating )
 
 # read and prepare source code from chosen file
 filename = sys.argv[1]
@@ -39,16 +61,28 @@ lines = sourceCode.split( ";" )
 # variables memory
 memory = {}
 
+# temporarily store functions and loops contents
+tempFunct = ""
+tempLoop = ""
+
 # read line by line and call functions for each
 for line in lines:
 	tokens = line.split( " " )
 	firstToken = tokens[0]
+
+	if firstToken == "^":
+		continue
+
+	if inLoop == True:
+		print( "looping!" )
+		continue
+
 	if firstToken == "loc" and tokens[2] == "=":
 		parseAssignVar( line )
-	elif firstToken == "^":
-		continue
 	elif firstToken == "print":
 		printStatement( line )
+	elif firstToken == "loop" and tokens[2] == "for":
+		startLoop( line )
 
 # check all the stored variables
-# print( memory )
+print( memory )
